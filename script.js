22d8693e9abc9900afccbea0262aecaa64d963a3
Adrian Hatfield <adrianhatfield23@gmail.com>
2026-09-12 12:29:00 +0100

    Fix issues

--- script.js
+++ script.js
@@ -40,22 +40,23 @@ function generateYears(data) {
         const h2 = document.createElement("h2");
         h2.setAttribute("href", "#" + thisYear.year);
 
-        const ulOne = document.createElement("ul");
-        const ulTwo = document.createElement("ul");
+        if (semOne.length > 0) {
+            const ulOne = document.createElement("ul");
+            generateSessions(semOne, ulOne);
+            const semOneTitle = document.createElement("h3");
+            semOneTitle.innerHTML = "Semester One";
+            yearsElement.appendChild(semOneTitle);
+            yearsElement.appendChild(ulOne);
+        }
 
-        generateSessions(semOne, ulOne);
-        generateSessions(semTwo, ulTwo);
-
-        const semOneTitle = document.createElement("h3");
-        semOneTitle.innerHTML = "Semester One";
-
-        const semTwoTitle = document.createElement("h3");
-        semTwoTitle.innerHTML = "Semester Two";
-
-        yearsElement.appendChild(semOneTitle);
-        yearsElement.appendChild(ulOne);
-        yearsElement.appendChild(semTwoTitle);
-        yearsElement.appendChild(ulTwo);
+        if (semTwo.length > 0) {
+            const ulTwo = document.createElement("ul");
+            generateSessions(semTwo, ulTwo);
+            const semTwoTitle = document.createElement("h3");
+            semTwoTitle.innerHTML = "Semester Two";
+            yearsElement.appendChild(semTwoTitle);
+            yearsElement.appendChild(ulTwo);
+        }
     }
 }
 
@@ -83,24 +84,29 @@ function toggleVisability(data) {
     h2.innerHTML = data.title;
     sessionInfoElement.appendChild(h2);
 
-    for (let i = 0; i < data.description.length; i++) {
-        const p = document.createElement("p");
-        p.innerHTML = data.description[i];
-        sessionInfoElement.appendChild(p);
+    if (data.description.length > 0) {
+        for (let i = 0; i < data.description.length; i++) {
+            const p = document.createElement("p");
+            p.innerHTML = data.description[i];
+            sessionInfoElement.appendChild(p);
+        }
     }
 
-    const h3 = document.createElement("h3");
-    h3.innerHTML = "Links";
-    sessionInfoElement.appendChild(h3);
+    if (data.links.length > 0) {
+        const h3 = document.createElement("h3");
+        h3.innerHTML = "Links";
+        sessionInfoElement.appendChild(h3);
 
-    for (let i = 0; i < data.links.length; i++) {
-        const p = document.createElement("p");
-        const a = document.createElement("a");
-        a.innerHTML = data.links[i];
-        a.setAttribute("href", data.links[i]);
-        p.appendChild(a);
-        sessionInfoElement.appendChild(p);
+        for (let i = 0; i < data.links.length; i++) {
+            const p = document.createElement("p");
+            const a = document.createElement("a");
+            a.innerHTML = data.links[i];
+            a.setAttribute("href", data.links[i]);
+            p.appendChild(a);
+            sessionInfoElement.appendChild(p);
+        }
     }
+    
 }
 
 loadJSON();
